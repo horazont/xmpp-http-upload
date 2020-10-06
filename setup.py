28c25b67b43b864ac04bd374a46135e78b4e84d0
Jonas Schäfer <j.wielicki@sotecware.net>
2020-10-06 18:11:21 +0200

    Bump version number to 0.4.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
 
 setup(
     name="xmpp-http-upload",
-    version="0.3.0",
+    version="0.4.0",
     description="Flask-based HTTP service to handle XMPP HTTP upload requests from Prosody mod_http_upload_external",
     long_description=long_description,
     url="https://github.com/horazont/xmpp-http-upload",
